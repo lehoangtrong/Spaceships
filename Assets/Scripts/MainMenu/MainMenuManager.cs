3e4bdc51e8a954f0a5ac0753c1eb6ab7f44b72e1
Lê Hoàng Trọng <lehoangtrongcsgl@gmail.com>
2025-05-25 23:16:38 +0700

    Feat: add some sprites

---
+++ Assets/Scripts/MainMenu/MainMenuManager.cs
@@ -0,0 +1,50 @@
+using UnityEngine;
+using UnityEngine.SceneManagement; // Thư viện quản lý cảnh
+
+public class MainMenuManager : MonoBehaviour
+{
+    // SerializeField cho phép gán giá trị trực tiếp mà không cần public 
+    [SerializeField] private GameObject instructionsPanel;
+
+    void Start()
+    {
+        // Ẩn Panel khi bắt đầu
+        instructionsPanel.SetActive(false);
+    }
+
+    public void StartGame()
+    {
+        Debug.Log("Start Game");
+        SceneManager.LoadScene("GamePlay"); // Tên cảnh cần load
+    }
+
+    public void ShowInstructions()
+    {
+        Debug.Log("Show Instructions");
+        if (instructionsPanel != null)
+        {
+            instructionsPanel.SetActive(true);
+        }
+        else
+        {
+            Debug.LogWarning("Instructions panel is not assigned in the inspector.");
+        }
+    }
+
+    public void HideInstructions()
+    {
+        if (instructionsPanel != null)
+        {
+            instructionsPanel.SetActive(false);
+        }
+        else
+        {
+            Debug.LogWarning("Instructions panel is not assigned in the inspector.");
+        }
+    }
+    public void QuitGame()
+    {
+        Debug.Log("Quit Game");
+        Application.Quit();
+    }
+}
